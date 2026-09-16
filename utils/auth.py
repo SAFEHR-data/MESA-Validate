c061b60831d93e757cc2a27ae837814e729558ab
Stefan Piatek <s.piatek@ucl.ac.uk>
2026-09-16 10:49:54 +0100

    Stop pageload if not logged in

--- utils/auth.py
+++ utils/auth.py
@@ -6,6 +6,10 @@ from pathlib import Path
 import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
+from streamlit.logger import get_logger
+
+logger = get_logger(__name__)
+
 
 with open(Path(__file__).parents[1] / 'auth.yaml', 'r') as file:
     config = yaml.load(file, Loader=yaml.loader.SafeLoader)
@@ -22,3 +26,13 @@ def require_login():
         authenticator.login()
     except Exception as e:
         st.error(e)
+    finally:
+        if st.session_state.get('authentication_status'):
+            # Add logout button
+            authenticator.logout()
+            username = st.session_state.get("name")
+            logger.info(f"Pageload by {username}")
+            st.write(f"Welcome *{username}*")
+        else: 
+            st.stop()
+
